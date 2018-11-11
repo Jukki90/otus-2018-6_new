6025e4459dce10c426cdf60325c9695eaae6cc79
YIVerbitskaya <juliaylanova@mail.ru>
2018-11-11 21:39:45 +0300

    * bugfix

--- L14/src/main/java/servlet/TemplateProcessor.java
+++ L14/src/main/java/servlet/TemplateProcessor.java
@@ -5,7 +5,6 @@ import freemarker.template.Template;
 import freemarker.template.TemplateException;
 import org.springframework.stereotype.Component;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.StringWriter;
 import java.io.Writer;
@@ -16,12 +15,11 @@ import java.util.Map;
  */
 @Component
 public class TemplateProcessor {
-    private static final String HTML_DIR = "public_html";
     private final Configuration configuration;
 
     public TemplateProcessor() throws IOException {
         configuration = new Configuration(Configuration.VERSION_2_3_28);
-        configuration.setDirectoryForTemplateLoading(new File(HTML_DIR));
+        configuration.setClassForTemplateLoading(this.getClass(), "/tml/");
         configuration.setDefaultEncoding("UTF-8");
     }
 
